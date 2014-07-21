2c30e255bcd3197fe474d1749799c216a2514217
laosb <laosb@lasteam.net>
2014-07-21 21:32:56 +0800

    Limit it by accounts.

--- best-sayings.js
+++ best-sayings.js
@@ -1,7 +1,11 @@
 sayings = new Meteor.Collection('sayings');
+Meteor.users.deny({update: function () { return true; }});
 Meteor.methods({
     likeIt:function(id){
+        if(!Meteor.user()){throw new Meteor.Error(401,'Access Denied. Log in first.');}
+        if((Meteor.user().voted==1)||(Meteor.user().voted==undefined)){throw new Meteor.Error(401,'Access Denied. You have voted');}
         sayings.update({_id:id},{$inc:{likes:1}});
+        Meteor.users.update({_id:Meteor.userId()},{$set:{voted:1}},{upsert:true});
     },
     addSaying:function(str){
         sayings.insert({content:str,likes:0});
@@ -13,7 +17,7 @@ if (Meteor.isClient) {
   };
   Template.saying.events({
     'click .likeit': function (e) {
-      Meteor.call('likeIt',$(e.target).attr('id'));
+      Meteor.call('likeIt',$(e.target).attr('id'),function(e,t){if(e){alert(e.reason)}});
     }
   });
   Template.addsaying.events({
@@ -22,3 +26,8 @@ if (Meteor.isClient) {
       }
   });
 }
+Accounts.onCreateUser(function(options, user) {
+  user.profile = options.profile;
+  user.voted = 0;
+  return user;
+}); 
