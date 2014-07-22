c182caf7522f11aa7c4487772ba0eb903cf38ff2
laosb <laosb@lasteam.net>
2014-07-22 08:45:35 +0800

    Allow 5 votes per user. Deny sayings shorter than 5 chars.

--- best-sayings.js
+++ best-sayings.js
@@ -1,13 +1,16 @@
 sayings = new Meteor.Collection('sayings');
-Meteor.users.deny({update: function () { return true; }});
+Meteor.users.deny({update: function () { return true; }}); //Deny all to keep safe.
 Meteor.methods({
     likeIt:function(id){
         if(!Meteor.user()){throw new Meteor.Error(401,'Access Denied. Log in first.');}
-        if((Meteor.user().voted==1)||(Meteor.user().voted==undefined)){throw new Meteor.Error(401,'Access Denied. You have voted');}
+        if(Meteor.user().voted>=5){throw new Meteor.Error(401,'Access Denied. You have voted 5 times. Every user can only vote 5 times.');}
         sayings.update({_id:id},{$inc:{likes:1}});
-        Meteor.users.update({_id:Meteor.userId()},{$set:{voted:1}},{upsert:true});
+        Meteor.users.update({_id:Meteor.userId()},{$inc:{voted:1}},{upsert:true});
     },
     addSaying:function(str){
+        if(str.replace(/ /gi,'').length<=5){
+            throw new Meteor.Error(503,'Your "saying" is too short. It must be longer than 5 characters, except spaces.')
+        } //Delete all spaces and count the length.
         sayings.insert({content:str,likes:0});
     }
 });
@@ -17,12 +20,12 @@ if (Meteor.isClient) {
   };
   Template.saying.events({
     'click .likeit': function (e) {
-      Meteor.call('likeIt',$(e.target).attr('id'),function(e,t){if(e){alert(e.reason)}});
+      Meteor.call('likeIt',$(e.target).attr('id'),function(e,t){if(e){alert(e.reason)}});//Last param is a callback function to catch the error and alert. The data was collected by jQuery.
     }
   });
   Template.addsaying.events({
       'click .addone':function(){
-          Meteor.call('addSaying',$('.addsaying').val());
+          Meteor.call('addSaying',$('.addsaying').val(),function(e,t){if(e){alert(e.reason)}});
       }
   });
 }
